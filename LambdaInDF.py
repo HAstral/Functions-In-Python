b58f0c66c612eea9c6fab3b6b7c52dab1bc678e6
htetaunghlyan13@gmail.com <htetaunghlyan13@gmail.com>
2025-11-21 13:39:13 +0630

    new py added

---
+++ LambdaInDF.py
@@ -0,0 +1,14 @@
+# Import pandas
+import pandas as pd
+
+# Import Twitter data as DataFrame: df
+tweets_df = pd.read_csv('tweets.csv')
+# Select retweets from the Twitter DataFrame: result
+result = filter(lambda x: x[0:2]=="RT", tweets_df['text'])
+
+# Create list from filter object result: res_list
+res_list=list(result)
+
+# Print all retweets in res_list
+for tweet in res_list:
+    print(tweet)
